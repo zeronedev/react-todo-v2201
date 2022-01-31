08a1fd6e7eda40fcec6a20b9db7db1e6281b1a21
zeronedev <ok7375@gmail.com>
2022-01-31 14:42:30 +0900

    #5.10 Recap: 다지우고 toDo 처음

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,13 +1,7 @@
 import { useForm } from "react-hook-form";
 
 interface IForm {
-  email: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  password: string;
-  password1: string;
-  extraError?: string;
+  toDo: string;
 }
 
 function ToDoList() {
@@ -15,88 +9,25 @@ function ToDoList() {
     register,
     handleSubmit,
     formState: { errors },
-    setError,
-  } = useForm<IForm>({ defaultValues: { email: "@naver.com" } });
-  const onValid = (data: IForm) => {
-    if (data.password !== data.password1) {
-      setError(
-        "password1",
-        { message: "패스워드가 같지 않습니다." },
-        { shouldFocus: true }
-      );
-    }
-    // setError("extraError", { message: "Server offline." });
+    setValue,
+  } = useForm<IForm>();
+  const handleValid = (data: IForm) => {
+    console.log("입력한 할일은 ", data.toDo);
+    setValue("toDo", "");
   };
   console.log(errors);
   return (
     <div>
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={handleSubmit(onValid)}
-      >
+      <form onSubmit={handleSubmit(handleValid)}>
         <input
-          {...register("email", {
-            required: "이메일은 필수 입력 입니다.",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "naver.com 메일만 가능 합니다.",
-            },
-          })}
-          placeholder="Email"
+          {...register("toDo", { required: "해야 할 일을 입력하세요." })}
+          placeholder="해야 할 일"
         />
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register("firstName", {
-            required: "필수 입력입니다",
-            validate: {
-              noNico: (value) =>
-                value.includes("nico") ? "nico 는 사용할수 없습니다" : true,
-              noNick: (value) =>
-                value.includes("nick") ? "nick 는 사용할수 없습니다" : true,
-            },
-          })}
-          placeholder="First Name"
-        />
-        <span>{errors?.firstName?.message}</span>
-        <input
-          {...register("lastName", { required: "필수 입력입니다" })}
-          placeholder="Last Name"
-        />
-        <span>{errors?.lastName?.message}</span>
-        <input
-          {...register("username", {
-            required: "필수 입력입니다.",
-            minLength: {
-              value: 10,
-              message: "10글자 이상 입력해주세요.",
-            },
-          })}
-          placeholder="Username"
-        />
-        <span>{errors?.username?.message}</span>
-        <input
-          {...register("password", {
-            required: "패스워드를 입력해 주세요.",
-            minLength: 5,
-          })}
-          placeholder="Password"
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register("password1", {
-            required: "패스워드를 입력해 주세요",
-            minLength: {
-              value: 5,
-              message: "패스워드 5글자 이상 입력해 주세요 .",
-            },
-          })}
-          placeholder="Password1"
-        />
-        <span>{errors?.password1?.message}</span>
+
         <button>Add</button>
-        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
 }
+
 export default ToDoList;
