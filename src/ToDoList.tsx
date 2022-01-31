c084de2ff9f8bcc6d23eecf54f0e9fc3196e3cc8
zeronedev <ok7375@gmail.com>
2022-01-31 09:17:31 +0900

    #5.6 Forms: ...register(),watch()

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,4 +1,20 @@
+import { useForm } from "react-hook-form";
+
 function ToDoList() {
-  return <div>To Do List</div>;
+  const { register, watch } = useForm();
+  console.log(watch());
+  return (
+    <div>
+      <form>
+        <input {...register("email")} placeholder="Email" />
+        <input {...register("firstName")} placeholder="First Name" />
+        <input {...register("lastName")} placeholder="Last Name" />
+        <input {...register("username")} placeholder="Username" />
+        <input {...register("password")} placeholder="Password" />
+        <input {...register("password1")} placeholder="Password1" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 export default ToDoList;
